fd21220bee3f201a69939f8aec2a3803f24388c9
Manasi Patil <patilm0204@gmail.com>
2025-07-27 10:52:14 -0400

    Array Q2 easy

--- Arrays/Longest_Common_Prefix.java
+++ Arrays/Longest_Common_Prefix.java
@@ -46,24 +46,6 @@ class Longest_Common_Prefix {
         }
         return answer;
     }
-
-
-    /* 
-    * Solution 2
-    */
-    public String longestCommonPrefix2(String[] strs) {
-        if(strs.length ==0 || strs==null)
-            return "";
-        String prefix = strs[0];
-        for(int i=1;i<strs.length;i++){
-            if(strs[i].indexOf(prefix)!=0){
-                prefix=prefix.substring(0, prefix.length()-1);
-                if(prefix.isEmpty())
-                    return "";
-            }
-        }
-        return prefix;
-    }
 }
 
 
