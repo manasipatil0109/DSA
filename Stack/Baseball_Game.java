6a8943a243acf36373af6129d634464faca8bec9
Manasi Patil <patilm0204@gmail.com>
2025-08-17 14:22:34 -0400

    game code

---
+++ Stack/Baseball_Game.java
@@ -0,0 +1,74 @@
+/*
+ * 682. Baseball Game
+
+You are keeping the scores for a baseball game with strange rules. At the beginning of the game, you start with an empty record.
+
+You are given a list of strings operations, where operations[i] is the ith operation you must apply to the record and is one of the following:
+
+An integer x.
+Record a new score of x.
+'+'.
+Record a new score that is the sum of the previous two scores.
+'D'.
+Record a new score that is the double of the previous score.
+'C'.
+Invalidate the previous score, removing it from the record.
+Return the sum of all the scores on the record after applying all the operations.
+
+The test cases are generated such that the answer and all intermediate calculations fit in a 32-bit integer and that all operations are valid.
+
+ 
+
+Example 1:
+
+Input: ops = ["5","2","C","D","+"]
+Output: 30
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"2" - Add 2 to the record, record is now [5, 2].
+"C" - Invalidate and remove the previous score, record is now [5].
+"D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+"+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+The total sum is 5 + 10 + 15 = 30.
+Example 2:
+
+Input: ops = ["5","-2","4","C","D","9","+","+"]
+Output: 27
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"-2" - Add -2 to the record, record is now [5, -2].
+"4" - Add 4 to the record, record is now [5, -2, 4].
+"C" - Invalidate and remove the previous score, record is now [5, -2].
+"D" - Add 2 * -2 = -4 to the record, record is now [5, -2, -4].
+"9" - Add 9 to the record, record is now [5, -2, -4, 9].
+"+" - Add -4 + 9 = 5 to the record, record is now [5, -2, -4, 9, 5].
+"+" - Add 9 + 5 = 14 to the record, record is now [5, -2, -4, 9, 5, 14].
+The total sum is 5 + -2 + -4 + 9 + 5 + 14 = 27.
+ */
+package Stack;
+
+ class Baseball_Game {
+    public int calPoints(String[] operations) {
+        int[] arr = new int[operations.length];
+        int top=-1;
+        for(int i=0;i<operations.length;i++){
+            String c = operations[i];
+            if(c.equals("C") && top!=-1){
+                top--;
+            } else if(c.equals("D")){
+                arr[top+1]=arr[top]*2;
+                top++;
+            } else if(c.equals("+")){
+                arr[top+1]=arr[top]+arr[top-1];
+                top++;
+            }else{
+                arr[++top]=Integer.parseInt(c);
+            }
+            System.out.println(top);
+        }
+        for(int i=1;i<=top;i++){
+            arr[0]+=arr[i];
+        }
+        return top==-1?0:arr[0];
+    }
+}
