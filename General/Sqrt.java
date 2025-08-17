c9c082277cd2fe7682b9ce3c50c2f9c202b237e4
Manasi Patil <patilm0204@gmail.com>
2025-08-17 19:10:16 -0400

    sqrt code

---
+++ General/Sqrt.java
@@ -0,0 +1,44 @@
+/*
+ * 69. Sqrt(x)
+
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+ */
+
+package General;
+
+public class Sqrt {
+
+    /*
+     * Solution 1
+     */
+    public int mySqrt(int x) {
+        if(x<1)
+            return 0;
+        if(x<4)
+            return 1;
+        for(int i=2;i<=x/2;i++){
+            long sq = (long) i * i;
+            long nextSq = (long) (i + 1) * (i + 1);
+            if (sq == x || (sq < x && nextSq > x)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
