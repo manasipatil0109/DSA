aa7b7aa8fced97b6db150ced9add953b1a7be3b7
Manasi Patil <patilm0204@gmail.com>
2025-08-20 10:06:29 -0400

    array medium code

---
+++ Arrays/Check_Array_Sorted_and_Rotated.java
@@ -0,0 +1,50 @@
+/*
+ * 1752. Check if Array Is Sorted and Rotated
+
+Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+
+There may be duplicates in the original array.
+
+Note: An array A rotated by x positions results in an array B of the same length such that B[i] == A[(i+x) % A.length] for every valid index i.
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: true
+Explanation: [1,2,3,4,5] is the original sorted array.
+You can rotate the array by x = 2 positions to begin on the element of value 3: [3,4,5,1,2].
+Example 2:
+
+Input: nums = [2,1,3,4]
+Output: false
+Explanation: There is no sorted array once rotated that can make nums.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: true
+Explanation: [1,2,3] is the original sorted array.
+You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
+ 
+ */
+
+package Arrays;
+
+public class Check_Array_Sorted_and_Rotated {
+    public boolean check(int[] nums) {
+        if(nums.length==1)
+            return true;
+        int flag=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]>nums[i]){
+                if(flag==1)
+                    return false;
+                else
+                    flag=1;
+            }
+        }
+        if(flag==1 && nums[nums.length-1]>nums[0]){
+            return false;
+        }
+        return true;
+    }
+}
