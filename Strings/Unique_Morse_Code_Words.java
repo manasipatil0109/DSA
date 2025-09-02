02503a831374451e09eff8e2d66932f321840279
Manasi Patil <patilm0204@gmail.com>
2025-09-01 22:37:53 -0400

    Unique Morse Code Words code

---
+++ Strings/Unique_Morse_Code_Words.java
@@ -0,0 +1,66 @@
+/*
+ * 804. Unique Morse Code Words
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+'a' maps to ".-",
+'b' maps to "-...",
+'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+Return the number of different transformations among all words we have.
+
+ 
+
+Example 1:
+
+Input: words = ["gin","zen","gig","msg"]
+Output: 2
+Explanation: The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations: "--...-." and "--...--.".
+Example 2:
+
+Input: words = ["a"]
+Output: 1
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 12
+words[i] consists of lowercase English letters.
+ */
+
+
+package Strings;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Unique_Morse_Code_Words {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] morseCode = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        Set<String> s = new HashSet<>();
+        for(String word: words){
+            StringBuilder sb = new StringBuilder(); // with string builder it is faster
+            for(char c:word.toCharArray()){
+                sb.append(morseCode[c-'a']);
+            }
+            s.add(sb.toString());
+        }
+        return s.size();
+
+    }
+}
