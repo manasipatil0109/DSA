c6c136f22f979b59664a934ee30a0ac271b05396
Manasi Patil <patilm0204@gmail.com>
2025-09-06 15:53:04 -0400

    Arranging Coins code

---
+++ General/Arranging_Coins.java
@@ -0,0 +1,44 @@
+/*
+ * 441. Arranging Coins
+
+You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+
+Given the integer n, return the number of complete rows of the staircase you will build.
+
+Example 1:
+Input: n = 5
+Output: 2
+Explanation: Because the 3rd row is incomplete, we return 2.
+
+Example 2:
+Input: n = 8
+
+Output: 3
+Explanation: Because the 4th row is incomplete, we return 3.
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+ */
+
+
+ package General;
+
+public class Arranging_Coins {
+    public int arrangeCoins(int n) {
+        long l=0;
+        long r=n;
+        while(l<=r){
+            long mid= l+(r-l)/2;
+            long count = mid*(mid+1)/2;
+            if(count==n)
+                return (int) mid;
+            else if(count<n)
+                l=mid+1;
+            else
+                r=mid-1;
+        }
+        return (int) r;
+    }
+}
