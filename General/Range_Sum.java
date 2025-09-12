6ecd924851b6d4ec3c055cb4646b1714ac6e63e3
Manasi Patil <patilm0204@gmail.com>
2025-09-11 23:36:10 -0400

    sum range code

---
+++ General/Range_Sum.java
@@ -0,0 +1,76 @@
+/*
+ * 303. Range Sum Query - Immutable
+
+Given an integer array nums, handle multiple queries of the following type:
+
+Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Implement the NumArray class:
+
+NumArray(int[] nums) Initializes the object with the integer array nums.
+int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+ 
+
+Example 1:
+Input
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output
+[null, 1, -1, -3]
+
+Explanation
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+ 
+Constraints:
+1 <= nums.length <= 104
+-105 <= nums[i] <= 105
+0 <= left <= right < nums.length
+At most 104 calls will be made to sumRange.
+
+ */
+
+ package General;
+
+ public class Range_Sum{
+
+    /*
+     * Solution 1
+     */
+    class NumArray {
+        int[] arr;
+        public NumArray(int[] nums) {
+            arr=nums;
+        }
+        
+        public int sumRange(int left, int right) {
+            int result=0;
+            for(int i=left;i<=right;i++){
+                result+=arr[i];
+            }
+            return result;
+        }
+    }
+
+    /*
+     * Solution 2 -  a bit faster
+     */
+    class NumArray1 {
+        int[] arr;
+        public NumArray1(int[] nums) {
+            arr= new int[nums.length];
+            arr[0]=nums[0];
+            for(int i=1;i<nums.length;i++){
+                arr[i]=arr[i-1]+nums[i];
+                System.out.println(arr[i]);
+            }
+        }
+        
+        public int sumRange(int left, int right) {
+            if(left==0)
+                return arr[right];
+            return arr[right]- arr[left-1];
+        }
+    }
+ }
