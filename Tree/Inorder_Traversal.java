c55bb82239db0211ec3d745aabb98247a3faaca8
Manasi Patil <patilm0204@gmail.com>
2025-10-31 13:15:00 -0400

    inorder code

---
+++ Tree/Inorder_Traversal.java
@@ -0,0 +1,70 @@
+/*
+ * 94. Binary Tree Inorder Traversal
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+Input: root = [1,null,2,3]
+
+Output: [1,3,2]
+
+Explanation:
+
+
+
+Example 2:
+
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+
+Output: [4,2,6,5,7,1,3,9,8]
+
+Explanation:
+
+
+
+Example 3:
+
+Input: root = []
+
+Output: []
+
+Example 4:
+
+Input: root = [1]
+
+Output: [1]
+
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ */
+
+
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inorder_Traversal {
+    /*
+     * Solution 1 - recursive
+     */
+    private void inorder(TreeNode root, List<Integer> l){
+        if(root==null)
+            return;
+        inorder(root.left, l);
+        l.add(root.val);
+        inorder(root.right, l);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> l = new ArrayList<>();
+        inorder(root, l);
+        return l;
+    }
+}
