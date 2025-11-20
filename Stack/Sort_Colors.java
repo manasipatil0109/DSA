66cb2059f04ca635506afdd0f340ff5915dfe0f1
Manasi Patil <patilm0204@gmail.com>
2025-11-20 16:54:12 -0500

    sort color code

--- Stack/Sort_Colors.java
+++ Stack/Sort_Colors.java
@@ -49,4 +49,31 @@ public class Sort_Colors {
             nums[j+1]=key;
         }
     }
+
+    public void sortColors3(int[] nums) {
+    int low = 0, mid = 0, high = nums.length - 1;
+
+    while (mid <= high) {
+        if (nums[mid] == 0) {
+            // swap nums[low] and nums[mid]
+            int temp = nums[low];
+            nums[low] = nums[mid];
+            nums[mid] = temp;
+
+            low++;
+            mid++;
+        }
+        else if (nums[mid] == 1) {
+            mid++;
+        }
+        else { // nums[mid] == 2
+            // swap nums[mid] and nums[high]
+            int temp = nums[mid];
+            nums[mid] = nums[high];
+            nums[high] = temp;
+
+            high--;
+        }
+    }
+}
 }
