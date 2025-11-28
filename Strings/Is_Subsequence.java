eff0184a018a06767f0ad2d96a7a3305d22a7eeb
Manasi Patil <patilm0204@gmail.com>
2025-11-28 18:58:43 -0500

    follow up code

--- Strings/Is_Subsequence.java
+++ Strings/Is_Subsequence.java
@@ -1,5 +1,10 @@
 package Strings;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Is_Subsequence {
     public boolean isSubsequence(String s, String t) {
         if(s.length()>t.length())
@@ -17,5 +22,60 @@ public class Is_Subsequence {
             return true;
         else
             return false;
+    }
+
+
+    /*
+    Solution for follow up question 
+    */
+    Map<Character, List<Integer>> map = new HashMap<>();
+
+    public boolean isSubsequence1(String s, String t) {
+
+        if (map.isEmpty()) {
+            for (int i = 0; i < t.length(); i++) {
+                char c = t.charAt(i);
+                map.computeIfAbsent(c, x -> new ArrayList<>()).add(i);
+            }
+        }
+
+        int prevIndex = -1;
+
+        for (char c : s.toCharArray()) {
+
+            // char does not exist in t
+            if (!map.containsKey(c)) 
+                return false;
+
+            List<Integer> positions = map.get(c);
+
+            // Use binary search to find smallest index > prevIndex
+            int nextIndex = findNext(positions, prevIndex);
+
+            if (nextIndex == -1)   
+                return false;
+
+            prevIndex = nextIndex;
+        }
+
+        return true;
+    }
+
+    // Binary search: first number > prevIndex
+    private int findNext(List<Integer> arr, int prevIndex) {
+        int left = 0, right = arr.size() - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr.get(mid) > prevIndex) {
+                result = arr.get(mid);
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return result;
     }
 }
