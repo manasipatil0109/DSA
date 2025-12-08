5ef98040b09516f7ea898f939497fdffe989fb46
Manasi Patil <patilm0204@gmail.com>
2025-12-08 18:41:13 -0500

    Maximum Depth of Binary Tree non recursive code

--- Tree/max_depth.java
+++ Tree/max_depth.java
@@ -16,12 +16,42 @@ Output: 2
 
 package Tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class max_depth {
+
+    /*
+    Solution 1 
+    */
     public int maxDepth(TreeNode root) {
         if(root==null)
             return 0;
         int l=maxDepth(root.left)+1;
         int r=maxDepth(root.right)+1;
         return l>r?l:r;
+    }
+
+    /*
+    Solution 2 
+    */
+   public int maxDepth1(TreeNode root) {
+        if(root == null)
+            return 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int level =0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            level++;
+            for(int i=0;i<size;i++){
+                TreeNode curr = q.poll();
+                if(curr.left != null)
+                    q.add(curr.left);
+                if(curr.right != null)
+                    q.add(curr.right);
+            } 
+        }
+        return level;
     }
 }
