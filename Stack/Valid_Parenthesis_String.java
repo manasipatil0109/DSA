4f43304c2511b3f1bc215e9c8b94f159d155e16d
Manasi Patil <patilm0204@gmail.com>
2025-12-16 10:20:16 -0500

    Valid Parenthesis String code

---
+++ Stack/Valid_Parenthesis_String.java
@@ -0,0 +1,69 @@
+/*
+678. Valid Parenthesis String
+
+Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+The following rules define a valid string:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "(*)"
+Output: true
+Example 3:
+
+Input: s = "(*))"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s[i] is '(', ')' or '*'. 
+*/
+
+
+
+package Stack;
+
+import java.util.Stack;
+
+public class Valid_Parenthesis_String {
+    public boolean checkValidString(String s) {
+        Stack<Integer> brac = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='(')
+                brac.push(i);
+            else if(c=='*')
+                star.push(i);
+            else{
+                if(!brac.isEmpty())
+                    brac.pop();
+                else if(!star.isEmpty())
+                    star.pop();
+                else
+                    return false;
+            }
+        }
+        while(!brac.isEmpty()){
+            int b = brac.pop();
+            if(star.isEmpty() || b > star.peek())
+                return false;
+            else
+                star.pop();
+        }
+        return true;
+    }
+}
