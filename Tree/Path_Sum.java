e9f1af89876b5f1e225b5223d1aa37184883de2b
Manasi Patil <patilm0204@gmail.com>
2026-01-18 17:11:13 -0500

    Path Sum I code

---
+++ Tree/Path_Sum.java
@@ -0,0 +1,57 @@
+/*
+112. Path Sum
+
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There are two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+Example 3:
+
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000 
+*/
+
+
+package Tree;
+
+public class Path_Sum {
+   /*
+   Solution 1 
+   */
+   public boolean hasPath(TreeNode root, int target, int currentSum){
+        if(root == null)
+            return false;
+        if(root.left == null && root.right == null)
+            return currentSum+root.val==target;
+        return hasPath(root.left, target, currentSum+root.val) || 
+                hasPath(root.right, target, currentSum+root.val);
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return hasPath(root, targetSum, 0);
+    } 
+}
