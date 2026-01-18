6a159e9e3275b7879808f8de0b8049b37be81708
Manasi Patil <patilm0204@gmail.com>
2026-01-18 17:11:25 -0500

    Subarray Sum Equals K brute force code

---
+++ Arrays/Subarray_Sum_Equals_K.java
@@ -0,0 +1,50 @@
+/*
+560. Subarray Sum Equals K
+
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107 
+*/
+
+
+
+package Arrays;
+
+public class Subarray_Sum_Equals_K {
+    /*
+    Solution 1 - TLE 
+    */
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            int sum = nums[i];
+            for(int j = i;j<nums.length;j++){
+                if(i!=j)
+                    sum+=nums[j];
+                if(sum == k){
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
