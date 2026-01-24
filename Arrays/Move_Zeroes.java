761997dbed686b3718d53244ee52eb2befca1cc9
Manasi Patil <patilm0204@gmail.com>
2026-01-24 14:51:41 -0500

    move zeroes faster code

--- Arrays/Move_Zeroes.java
+++ Arrays/Move_Zeroes.java
@@ -51,4 +51,18 @@ public class Move_Zeroes {
             }
         }
     }
+
+    /*
+    Solution 3 - faster
+    */
+    public void moveZeroes2(int[] nums) {
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp = nums[i];
+                nums[i]= nums[j];
+                nums[j++]=temp;
+            }
+        }
+    }
 }
